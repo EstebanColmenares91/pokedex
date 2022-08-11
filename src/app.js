3df5319bf4cf3403f3584d19fff76b9d23c9c8e9
Esteban Colmenares <estebancode91@gmail.com>
2022-08-11 14:40:28 -0400

    Reutilización del código de la función createPokemon

--- src/app.js
+++ src/app.js
@@ -27,12 +27,14 @@ const typeColor = {
   water: "#0190FF",
 };
 
+/* UTILS */
+
 function buscarPokemon() {
   const nombreID = document.getElementById("nombrePokemon");
   const nombreIDValue = nombreID.value;
   const nameToLowerCase = nombreIDValue.toLowerCase();
   const nameToLowerCaseTrimed = nameToLowerCase.trim();
-
+  
   if (nameToLowerCaseTrimed.length === 0) {
     spanWarning.innerText = "Este campo se encuentra vacío";
   } else {
@@ -42,73 +44,40 @@ function buscarPokemon() {
   }
 }
 
-async function fetchPokemonName(nombre) {
-  try {
-    const res = await fetch(API_URL_FETCH_POKEMON_NAME(nombre));
-
-    if (res.status === 404) {
-      spanError.innerText = "POKEMON NO ENCONTRADO INTENTE DE NUEVO";
-    } else {
-      const data = await res.json();
-      createPokemonSearched(data);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function createPokemonSearched(pokemon) {
-  pokemonName.innerHTML = "";
-
-  const card = document.createElement("div");
-  card.classList.add("pokemon-block");
-
-  const spriteContainer = document.createElement("div");
-  spriteContainer.classList.add("img-container");
-
-  const sprite = document.createElement("img");
-  sprite.src = pokemon.sprites.front_default;
-
-  spriteContainer.appendChild(sprite);
-
-  const number = document.createElement("p");
-  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-
-  const name = document.createElement("p");
-  name.innerText = pokemon.name.toUpperCase();
-
-  card.addEventListener("click", () => {
-    location.hash = "#pokemon=" + pokemon.name;
-    location.reload();
-  });
-
-  card.append(spriteContainer, number, name);
-  pokemonName.appendChild(card);
-}
-
-async function fetchPokemon(id) {
-  try {
-    const res = await fetch(API_URL_FETCH_POKEMON(id));
-
-    if (res.status !== 200) {
-      console.log("nossirbe");
-    } else {
-      const data = await res.json();
-      createPokemon(data);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 async function fetchPokemons(offset, limit) {
   for (let index = offset; index < offset + limit; index++) {
     await fetchPokemon(index);
   }
 }
 
-function createPokemon(pokemon) {
-  containerPrincipal.innerHTML = "";
+/* PÁGINA ANTERIOR */
+previous.addEventListener("click", () => {
+  if (offset != 1) {
+    offset -= 75;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+  } else {
+    spanError.innerText = "NO PUEDES SEGUIR RETROCEDIENDO";
+  }
+});
+
+/* PÁGINA SIGUIENTE */
+next.addEventListener("click", () => {
+  offset += 75;
+  removeChildNodes(pokemonContainer);
+  fetchPokemons(offset, limit);
+});
+
+/* REMOVER NODOS DE LA PÁGINA ANTERIOR */
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+/* UTILS CREATE POKEMON CON MANIPULACIÓN DEL DOM */
+function createPokemon(pokemon, childContainer, parentContainer) {
+  parentContainer.innerHTML = "";
 
   const card = document.createElement("div");
   card.classList.add("pokemon-block");
@@ -134,17 +103,8 @@ function createPokemon(pokemon) {
   });
 
   card.append(spriteContainer, number, name);
-  pokemonContainer.appendChild(card);
-  containerPrincipal.append(pokemonContainer);
-}
-
-async function selectedPokemonByClick(pokemonSelected) {
-  const res = await fetch(API_URL_FETCH_POKEMON_NAME(pokemonSelected));
-  const data = await res.json();
-
-  const pokemon = data;
-
-  createselectedPokemonByClick(pokemon);
+  childContainer.appendChild(card);
+  parentContainer.append(childContainer);
 }
 
 function createselectedPokemonByClick(pokemon) {
@@ -208,26 +168,48 @@ function createselectedPokemonByClick(pokemon) {
   pokedex2.appendChild(pokemonCard);
 }
 
-previous.addEventListener("click", () => {
-  if (offset != 1) {
-    offset -= 75;
-    removeChildNodes(pokemonContainer);
-    fetchPokemons(offset, limit);
-  } else {
-    spanError.innerText = "NO PUEDES SEGUIR RETROCEDIENDO";
-  }
-});
+/* LLAMADAS A LA API */
+async function fetchPokemonName(nombre) {
+  try {
+    const res = await fetch(API_URL_FETCH_POKEMON_NAME(nombre));
 
-next.addEventListener("click", () => {
-  offset += 75;
-  removeChildNodes(pokemonContainer);
-  fetchPokemons(offset, limit);
-});
-
-function removeChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+    if (res.status === 404) {
+      spanError.innerText = "POKEMON NO ENCONTRADO INTENTE DE NUEVO";
+    } else {
+      const data = await res.json();
+      
+      createPokemon(data, pokemonName, searchedPokemon__container);
+      console.log('llamando funcion');
+    }
+  } catch (error) {
+    console.error(error);
   }
+}
+
+
+async function fetchPokemon(id) {
+  try {
+    const res = await fetch(API_URL_FETCH_POKEMON(id));
+
+    if (res.status !== 200) {
+      console.log("nossirbe");
+    } else {
+      const data = await res.json();
+      const pokemon = data
+      createPokemon(pokemon, pokemonContainer, containerPrincipal);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function selectedPokemonByClick(pokemonSelected) {
+  const res = await fetch(API_URL_FETCH_POKEMON_NAME(pokemonSelected));
+  const data = await res.json();
+
+  const pokemon = data;
+
+  createselectedPokemonByClick(pokemon);
 }
 
 fetchPokemons(offset, limit);
